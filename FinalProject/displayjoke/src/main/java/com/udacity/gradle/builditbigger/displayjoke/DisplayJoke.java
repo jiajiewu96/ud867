e357b808940ff83cf02ff7d787c20c3eb8fb7f6c
jiajie <jiajie.wu96@gmail.com>
2019-09-20 08:29:00 -0700

    Saved instance state for joke

--- FinalProject/displayjoke/src/main/java/com/udacity/gradle/builditbigger/displayjoke/DisplayJoke.java
+++ FinalProject/displayjoke/src/main/java/com/udacity/gradle/builditbigger/displayjoke/DisplayJoke.java
@@ -1,11 +1,16 @@
 package com.udacity.gradle.builditbigger.displayjoke;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.widget.TextView;
 
 public class DisplayJoke extends AppCompatActivity {
+
+    private String mJoke = "There's no joke available";
+    private String mKey;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,10 +18,20 @@ public class DisplayJoke extends AppCompatActivity {
         setContentView(R.layout.activity_display_joke);
         TextView jokeLineTextView = findViewById(R.id.tv_joke);
         Bundle intentExtras = getIntent().getExtras();
+        mKey = getResources().getString(R.string.JOKE_KEY);
         if(intentExtras != null){
-            String punchLine = intentExtras.getString(getResources().getString(R.string.JOKE_KEY));
-            jokeLineTextView.setText(punchLine);
-
+            mJoke = intentExtras.getString(mKey);
+        } else if(savedInstanceState != null){
+            mJoke = savedInstanceState.getString(mKey);
+        } else{
+            jokeLineTextView.setText(mJoke);
         }
+
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
+        outState.putString(mKey, mJoke);
+        super.onSaveInstanceState(outState, outPersistentState);
     }
 }
